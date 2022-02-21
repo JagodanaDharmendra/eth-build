e932f18b9828756c8713daeee97013eaaa66cb59
DharmendraKumar Jagodana <47411494+JagodanaDharmendra@users.noreply.github.com>
2022-02-21 04:07:59 -0500

    feat: add Navbar component (#9)

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from "./appicon";
 export * from "./container";
 export * from "./button";
 export * from "./video-description-panel";
+export * from "./nav-bar";
---
+++ src/components/nav-bar/Navbar.type.tsx
@@ -0,0 +1,3 @@
+export default interface INavbarProps {
+  menus: Array<{ label: string; href: string }>;
+}
