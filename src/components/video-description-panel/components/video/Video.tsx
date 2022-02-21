272391127252e3db8382cb0f8a0293b5f705dbff
DharmendraKumar Jagodana <jagodana.dharmendra@gmail.com>
2022-02-21 06:32:50 -0500

    fix: update youtube video prop value

--- src/components/video-description-panel/components/video/Video.tsx
+++ src/components/video-description-panel/components/video/Video.tsx
@@ -5,7 +5,7 @@ function Video({ src, title }: IVideoProps) {
     <div className="max-w-3xl h-96 justify-center items-center content-center p-4">
       <iframe
         src={src}
-        allow="autoplay; encrypted-media"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
         title={String(title)}
         width={"100%"}
         height={"100%"}
