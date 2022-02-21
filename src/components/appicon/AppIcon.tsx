244c46a1e872411ddcd2ffd27f04c2a9f70b2378
DharmendraKumar Jagodana <jagodana.dharmendra@gmail.com>
2022-02-21 05:42:11 -0500

    fix: add nav-bar title

--- src/components/appicon/AppIcon.tsx
+++ src/components/appicon/AppIcon.tsx
@@ -1,5 +1,10 @@
 function AppIcon() {
-  return <span className="w-44">🛠️</span>;
+  return (
+    <div className="text-white font-semibold text-xl space-x-3">
+      <span className="w-44">🛠️</span>
+      <span>ETH.Build</span>
+    </div>
+  );
 }
 
 export default AppIcon;
